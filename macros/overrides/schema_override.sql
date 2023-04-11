99c61389e57cee0e8b7cf0917ff712f1845371c9
Steve Dowling <steve.dowling@fishtownanalytics.com>
2023-04-11 20:42:29 +0000

    updates

---
+++ macros/overrides/schema_override.sql
@@ -0,0 +1,14 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+
+        'dbt_sdowling_feature_demo'
+
+    {%- else -%}
+
+        'dbt_sdowling_feature_demo'
+
+    {%- endif -%}
+
+{%- endmacro %}
--- models/examples/data_science_workflow/forecast_reporting_table.sql
+++
@@ -1,9 +0,0 @@
-with forecast_daily_returns as (
-
-    Select * from {{ ref('forecast_daily_returns') }}
-)
-
-
-Select 
-*
-from forecast_daily_returns
