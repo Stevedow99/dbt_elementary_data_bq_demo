71d1499347b4fb55061ba25df8cd05f38575cb43
Steve Dowling <steve.dowling@fishtownanalytics.com>
2023-04-12 01:08:25 +0000

    updates

--- models/examples/track_metrics_over_time/2__metrics_point_in_time_table/metrics_point_in_time_table.sql
+++ models/examples/track_metrics_over_time/2__metrics_point_in_time_table/metrics_point_in_time_table.sql
@@ -5,6 +5,8 @@
     )
 }}
 
+-- in this case we are talking the {{ ref('new_daily_subscribers_metric_calculation') }} and {{ ref('subscriber_count_metric_calculation') }} into the union
+
 
 -- get all models that end in '_metric_calculation'
 {% set metric_calc_relations = dbt_utils.get_relations_by_pattern(this.schema, '%_metric_calculation') %}
